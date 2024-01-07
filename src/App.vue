a080df7ad47fc0da305ef40f6b71557cb633a27c
emon <liming20110711@163.com>
2024-01-07 09:07:27 +0800

    test: dd

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>我是App</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+const str = '我爱你祖国'
+</script>
+
+<style lang="scss" scoped>
+.box {
+  width: 600px;
+  height: 400px;
+  background-color: black;
+
+  h1 {
+    color: white;
+  }
+}
+</style>
