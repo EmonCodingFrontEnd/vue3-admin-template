39fd0ccbabdc1a0cd8d47926b411dc8e67ac5285
emon <liming20110711@163.com>
2024-02-15 10:11:41 +0800

    perf: 美化登录页2

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -159,6 +159,7 @@ const login = async () => {
     width: 80%;
     position: relative;
     top: 30vh;
+    margin: auto;
     background: url('@/assets/images/login_form.png') no-repeat;
     background-size: cover;
     padding: 40px;
