838d34d8f01c3a649dadc10b4fa16c85101d2dfe
emon <liming20110711@163.com>
2024-02-19 13:35:11 +0800

    feat: 中间部分初次提交

---
+++ src/views/screen/components/center/line/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="line_box">我是折线组件</div>
+</template>
+
+<script setup lang="ts" name="Line"></script>
+
+<style scoped lang="scss">
+.line_box {
+  background-color: purple;
+}
+</style>
---
+++ src/views/screen/components/center/map/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="map_box" ref="charts">我是地图组件</div>
+</template>
+
+<script setup lang="ts" name="Map">
+import { onMounted, ref } from 'vue'
+import * as echarts from 'echarts'
+
+// 定义引用
+const charts = ref()
+onMounted(() => {
+  // 获取echarts类的实例对象
+  const myChart = echarts.init(charts.value)
+  // 设置实例的配置项
+  myChart.setOption({})
+})
+</script>
+
+<style scoped lang="scss">
+.map_box {
+  background-color: pink;
+}
+</style>
